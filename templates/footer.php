f3f80ae8e49710bd85156d816ae0b62ebd53211a
shamim2883 <shamim_biplob@yahoo.com>
2016-10-31 18:56:36 +0600

    templates added

---
+++ templates/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+
+?>
+
+		</div><!--#fep-content -->
+	  
+	<div id='fep-footer'>
+
+	<?php do_action('fep_footer_note'); ?>
+
+	</div><!--#fep-footer -->
+</div><!--#fep-wrapper -->
+
---
+++ templates/menu.php
@@ -0,0 +1,15 @@
+<?php
+
+
+?>
+
+	  
+<div id='fep-menu'>
+
+	<?php do_action('fep_menu_button'); ?>
+
+</div><!--#fep-menu -->
+<div id='fep-content'>
+	  
+	<?php do_action('fep_display_before_content'); ?>
+
